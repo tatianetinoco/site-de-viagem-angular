c336d3d06121bceb51ebb6e3dab674f4238c8040
tatianetinoco <tatianeperr@gmail.com>
2024-02-24 22:23:53 -0300

    Site De Viagem Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'site-de-viagem-angular';
+  title = 'Site De Viagem Angular';
 }
