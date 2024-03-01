403d2b647f831bc5c5d65de36d106f9ffc1fd117
tatianetinoco <tatianeperr@gmail.com>
2024-03-01 00:14:57 -0300

    componente why choose us

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { WhyChooseUsComponent } from '../../components/why-choose-us/why-choose-us.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, WhyChooseUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
