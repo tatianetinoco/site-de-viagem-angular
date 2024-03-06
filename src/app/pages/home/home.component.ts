f1a9c41cbb43c9bbb0f11497f393ce08de3a5d52
tatianetinoco <tatianeperr@gmail.com>
2024-03-05 22:31:06 -0300

    add cards continentes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { WhyChooseUsComponent } from '../../components/why-choose-us/why-choose-us.component';
+import { CardsComponent } from '../../components/cards/cards.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, WhyChooseUsComponent],
+  imports: [HeaderComponent, FooterComponent, WhyChooseUsComponent, CardsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
