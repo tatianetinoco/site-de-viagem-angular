e46bf37eb34e2d02da5d6e90ce4df67fbcbd987b
tatianetinoco <tatianeperr@gmail.com>
2024-03-19 00:54:44 -0300

    página Sobre e add atualização home

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { MenuComponent } from '../../components/menu/menu.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, MenuComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { WhyChooseUsComponent } from '../../components/why-choose-us/why-choose-us.component';
 import { CardsComponent } from '../../components/cards/cards.component';
+import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, WhyChooseUsComponent, CardsComponent],
+  imports: [HeaderComponent, FooterComponent, WhyChooseUsComponent, CardsComponent, TestimonialsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
