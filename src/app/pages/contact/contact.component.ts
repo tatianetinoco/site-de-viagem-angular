2ac86cae49a3837a363842635eaace79609ac4c1
tatianetinoco <tatianeperr@gmail.com>
2024-03-19 16:49:09 -0300

    formulário de contato

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../../components/menu/menu.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { FormComponent } from '../../components/form/form.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, FooterComponent, FormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
