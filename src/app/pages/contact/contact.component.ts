41de860877d64b472ce0c576d53c770e1089d639
tatianetinoco <tatianeperr@gmail.com>
2024-03-19 16:55:49 -0300

    add header

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MenuComponent } from '../../components/menu/menu.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { FormComponent } from '../../components/form/form.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [MenuComponent, FooterComponent, FormComponent],
+  imports: [MenuComponent, FooterComponent, FormComponent, HeaderComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
